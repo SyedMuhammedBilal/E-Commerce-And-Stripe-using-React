ef47a444a1a67bf133dd2d5b5927995c774b0887
Syed Muhammed Bilal <intergo878@gmail.com>
2020-11-23 15:13:58 +0500

    new components

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVO3XcKajfr5zhZ66D87mWOvpMC0Zm1pw",
+    authDomain: "clone-74b77.firebaseapp.com",
+    databaseURL: "https://clone-74b77.firebaseio.com",
+    projectId: "clone-74b77",
+    storageBucket: "clone-74b77.appspot.com",
+    messagingSenderId: "660493862944",
+    appId: "1:660493862944:web:927de679e935132cfe9445"
+};
